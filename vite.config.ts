c95df697838dc54017f27bd7647e6213945226c9
planc <hubenchang0515@outlook.com>
2025-01-06 16:07:11 +0800

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Shift - Online Code Interpreter',
+        short_name: 'Shift - Online Code Interpreter',
+        description: 'Edit and run code script online - 在线编辑和运行代码脚本',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'PWA/icon-192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+            purpose: "any"
+          },
+          {
+            src: 'PWA/icon-512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 16 * 1024 ** 2, // 5 MB or set to something else
+      }
+    }),
+  ],
   base: '/shift/'
 })
