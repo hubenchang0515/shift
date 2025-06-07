1d445e49ccf6782e86c60dbd4e518ce16044d06e
Plan C <hubenchang0515@outlook.com>
2025-06-08 02:09:09 +0800

    恢复自动聚焦

--- src/components/HighlightEditor.tsx
+++ src/components/HighlightEditor.tsx
@@ -130,6 +130,7 @@ export function HighlightEditor(props:HighlightEditorProps, ref?:Ref<HTMLDivElem
                 }}
                 wrap="soft"
                 spellCheck="false"
+                autoFocus
                 onChange={refresh}
                 onKeyDown={onKeyDown}
                 onScroll={() => {
