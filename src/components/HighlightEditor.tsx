17fc3dd902856cbbf8836df9eedd279350fcc7ff
Plan C <hubenchang0515@outlook.com>
2025-06-09 15:15:10 +0800

    Revert "修复自动聚焦时发生意外滚动的问题"
    This reverts commit b42ff34b936dca4db8ff8922c627b2367d29fd54.

--- src/components/HighlightEditor.tsx
+++ src/components/HighlightEditor.tsx
@@ -71,11 +71,6 @@ export function HighlightEditor(props:HighlightEditorProps, ref?:Ref<HTMLDivElem
         refresh();
     }, [props.language])
 
-    // 自动聚焦
-    useEffect(() => {
-        inputRef.current?.focus({ preventScroll: true });
-    }, [inputRef])
-
     const commonStyle:CSSProperties = {
         margin: 0,
         padding: 12,
@@ -135,6 +130,7 @@ export function HighlightEditor(props:HighlightEditorProps, ref?:Ref<HTMLDivElem
                 }}
                 wrap="soft"
                 spellCheck="false"
+                autoFocus
                 onChange={refresh}
                 onKeyDown={onKeyDown}
                 onScroll={() => {
