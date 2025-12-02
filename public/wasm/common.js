d0b534a2edb5302465adca7cbd034f855eb60a6a
Plan C <hubenchang0515@outlook.com>
2025-12-03 01:41:29 +0800

    输出拆分

--- public/wasm/common.js
+++ public/wasm/common.js
@@ -2,13 +2,15 @@
  * Create the config of interpreter
  * @param args command line args, default
  * @param {string} code the code t orun
- * @param {(string)=>void} callback callback of output
+ * @param {(string, string, string)=>void} callback callback of STDOUT, STDERR and Mix-Output
  * @param {string} input string data of STDIN, default '' (empty)
  * @param {string[]} args command line args, default ['/tmp/code']
  * @returns the config
  */
 function makeConfig(code, callback, input='', args=['/tmp/code']) {
-    let outputData = [];
+    let stdoutData = [];
+    let stderrData = [];
+    let mixData = [];
     return {
         arguments: [...args],
         locateFile: (path, scriptDirectory) => {
@@ -26,21 +28,32 @@ function makeConfig(code, callback, input='', args=['/tmp/code']) {
                 let i = 0;
 
                 function stdin() {
+                    if (bytes.length === 0) {
+                        const lines = new TextDecoder('utf-8').decode(new Uint8Array(stdoutData)).split('\n');
+                        const msg = lines.length > 0 ? lines[lines.length - 1] : ""
+                        bytes = encoder.encode(window.prompt(msg||"STDIN")??"");
+                    }
+
                     if (i < bytes.length) {
                         const utf8Code = bytes[i];
+                        mixData.push(utf8Code)
                         i += 1;
                         return utf8Code
+                    } else if (i == bytes.length) {
+                        mixData.push(10);
+                        i += 1;
+                        return null
                     } else {
                         return null
                     }
                 }
 
                 function stdout(utf8Code) {
-                    outputData.push(utf8Code);
+                    stdoutData.push(utf8Code);
                 }
 
                 function stderr(utf8Code) {
-                    outputData.push(utf8Code);
+                    stderrData.push(utf8Code);
                 }
 
                 module.FS.init(stdin, stdout, stderr);
@@ -49,7 +62,8 @@ function makeConfig(code, callback, input='', args=['/tmp/code']) {
 
         postRun: [
             () => {
-                callback(new TextDecoder('utf-8').decode(new Uint8Array(outputData)));
+                const decoder = new TextDecoder('utf-8');
+                callback(decoder.decode(new Uint8Array(stdoutData)), decoder.decode(new Uint8Array(stderrData)), decoder.decode(new Uint8Array(mixData)));
             }
         ],
     }
